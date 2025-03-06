aef6b6891b7933885968928e1e1ead194a3ec232
CarlosAndre2001ws <163556831+CarlosAndre2001ws@users.noreply.github.com>
2025-03-06 09:45:25 -0300

    Blade Components

--- resources/views/username.blade.php
+++ resources/views/username.blade.php
@@ -1,55 +1,55 @@
 @extends('layouts.app')
 @section('content')
 
-Hi, {{ $user }}
-<br>
-You are a {{ $occupation }}
-<br>
-@if($records === 111)
-    I have 111 records
-@else
-    I have no records
-@endif
-<br>
-@unless(Auth::check())
-    Youre not logged in
-@endunless
-<br>
-@isset($records)
-    records are not null
-@endisset
-<br>
-@empty($heart)
-    your heart is empty
-@endempty
-<br>
-@auth(Auth::check())
-    I'm invisible until you are logged in
-@endauth
+{{--Hi, {{ $user }}--}}
+{{--<br>--}}
+{{--You are a {{ $occupation }}--}}
+{{--<br>--}}
+{{--@if($records === 111)--}}
+{{--    I have 111 records--}}
+{{--@else--}}
+{{--    I have no records--}}
+{{--@endif--}}
+{{--<br>--}}
+{{--@unless(Auth::check())--}}
+{{--    Youre not logged in--}}
+{{--@endunless--}}
+{{--<br>--}}
+{{--@isset($records)--}}
+{{--    records are not null--}}
+{{--@endisset--}}
+{{--<br>--}}
+{{--@empty($heart)--}}
+{{--    your heart is empty--}}
+{{--@endempty--}}
+{{--<br>--}}
+{{--@auth(Auth::check())--}}
+{{--    I'm invisible until you are logged in--}}
+{{--@endauth--}}
 
-@guest(Auth::check())
-    I'm invisible until you're not logged in
-@endguest
-<br>
-@switch($i)
-    @case(1)
-        First case...
-        @break
+{{--@guest(Auth::check())--}}
+{{--    I'm invisible until you're not logged in--}}
+{{--@endguest--}}
+{{--<br>--}}
+{{--@switch($i)--}}
+{{--    @case(1)--}}
+{{--        First case...--}}
+{{--        @break--}}
 
-    @case(2)
-        Second case...
-        @break
+{{--    @case(2)--}}
+{{--        Second case...--}}
+{{--        @break--}}
 
-    @default
-        Default case...
-@endswitch
-<br>
-@for($i = 0; $i < 10; $i++ )
-    The current value is {{ $i }} <br>
-@endfor
-<br>
+{{--    @default--}}
+{{--        Default case...--}}
+{{--@endswitch--}}
+{{--<br>--}}
+{{--@for($i = 0; $i < 10; $i++ )--}}
+{{--    The current value is {{ $i }} <br>--}}
+{{--@endfor--}}
+{{--<br>--}}
 
-@foreach( $users as $user)
+{{--@foreach( $users as $user)--}}
 {{--    While iterating through a foreach loop, a $loop variable will be available inside of your loop.--}}
 {{--    This variable provides access to some useful bits of information such as the current loop index--}}
 {{--    and whether this is the first of last iteration through the loop: --}}
@@ -71,45 +71,47 @@ You are a {{ $occupation }}
 {{--$loop->odd 	        Whether this is an odd iteration through the loop.--}}
 {{--$loop->depth 	    The nesting level of the current loop.--}}
 {{--$loop->parent 	    When in a nested loop, the parent's loop variable.--}}
-    @if($loop->first)
-        this is the first loop <br>
-    @endif
-    {{ $user }}
-    @if($loop->last)
-        <br>this is the last loop <br>
-    @endif
-@endforeach
+{{--    @if($loop->first)--}}
+{{--        this is the first loop <br>--}}
+{{--    @endif--}}
+{{--    {{ $user }}--}}
+{{--    @if($loop->last)--}}
+{{--        <br>this is the last loop <br>--}}
+{{--    @endif--}}
+{{--@endforeach--}}
 
-@php
-    $isActive = false;
-    $hasError = true;
-@endphp
+{{--@php--}}
+{{--    $isActive = false;--}}
+{{--    $hasError = true;--}}
+{{--@endphp--}}
 
-<div @class([
-    'p-4',
-    'font-bold' => $isActive,
-    'text-gray-500' => ! $isActive,
-    'bg-warning' => $hasError,
-])>hello</div>
+{{--<div @class([--}}
+{{--    'p-4',--}}
+{{--    'font-bold' => $isActive,--}}
+{{--    'text-gray-500' => ! $isActive,--}}
+{{--    'bg-warning' => $hasError,--}}
+{{--])>hello</div>--}}
 
-<div class="p-4 text-gray-500 bg-info">oi</div>
+{{--<div class="p-4 text-gray-500 bg-info">oi</div>--}}
 
-<div>
-    <label for="active">be</label>
-    <input
-        type="checkbox"
-        name="active"
-        value="active"
-        @checked(old('active'))
-    />
-</div>
-    <div>
-        <select name="version">
-            @foreach ($users as $user)
-                <option value="{{ $user }}" @selected(old('version') == $user)>
-                    {{ $user }}
-                </option>
-            @endforeach
-        </select>
-    </div>
+{{--<div>--}}
+{{--    <label for="active">be</label>--}}
+{{--    <input--}}
+{{--        type="checkbox"--}}
+{{--        name="active"--}}
+{{--        value="active"--}}
+{{--        @checked(old('active'))--}}
+{{--    />--}}
+{{--</div>--}}
+{{--    <div>--}}
+{{--        <select name="version">--}}
+{{--            @foreach ($users as $user)--}}
+{{--                <option value="{{ $user }}" @selected(old('version') == $user)>--}}
+{{--                    {{ $user }}--}}
+{{--                </option>--}}
+{{--            @endforeach--}}
+{{--        </select>--}}
+{{--    </div>--}}
+{{-- Blade Components --}}
+    <x-alert></x-alert>
 @endsection
