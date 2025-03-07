8314d74819546f099a29d95b6b1c76a623579b3e
CarlosAndre2001ws <163556831+CarlosAndre2001ws@users.noreply.github.com>
2025-03-07 10:04:05 -0300

    Accessing Parent Data

--- resources/views/username.blade.php
+++ resources/views/username.blade.php
@@ -122,6 +122,12 @@
                 @endforeach
             </select>
         </div>
-
     <x-forms.input></x-forms.input>
+
+    <x-menu color="purple">
+        <x-menu.item class="item">teste1</x-menu.item>
+        <x-menu.item class="item">teste2</x-menu.item>
+        <x-menu.item class="item">teste3</x-menu.item>
+        <x-menu.item class="item">teste4</x-menu.item>
+    </x-menu>
 @endsection
