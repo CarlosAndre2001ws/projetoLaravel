32121626f3cff63ca30be6bc543907edcd9355ca
CarlosAndre2001ws <163556831+CarlosAndre2001ws@users.noreply.github.com>
2025-03-12 13:22:41 -0300

    Authorizing Form Requests

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -5,7 +5,9 @@ namespace App\Http\Controllers;
 use App\Http\Requests\StorePostRequest;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\View\View;
+use App\Models\User;
 
 class PostController extends Controller
 {
@@ -16,9 +18,13 @@ class PostController extends Controller
         return view('dev');
     }
 
-    public function secondValidation(Request $request): View
+    public function secondValidation(StorePostRequest $request): View
     {
+        $user = Auth::user();
+
         $validated = $request->validated();
+
         return view('dev');
     }
+
 }
--- app/Http/Requests/StorePostRequest.php
+++ app/Http/Requests/StorePostRequest.php
@@ -10,20 +10,20 @@ class StorePostRequest extends FormRequest
      * Determine if the user is authorized to make this request.
      */
 
-    /**
-
-     * The URI that users should be redirected to if validation fails.
-
-     *
-
-     * @var string
-
-     */
+//    /**
+//
+//     * The URI that users should be redirected to if validation fails.
+//
+//     *
+//
+//     * @var string
+//
+//     */
 //    protected $redirect = '/login';
 
     public function authorize(): bool
     {
-        return true;
+        return $this->user()->can('storePost');
     }
 
     /**
