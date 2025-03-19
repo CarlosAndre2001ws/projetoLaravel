77dac808a0612bf91ae65ff181596ebbd9e59bbb
CarlosAndre2001ws <163556831+CarlosAndre2001ws@users.noreply.github.com>
2025-03-19 13:29:21 -0300

    Indexes

--- database/migrations/2025_03_19_162811_adding_index_column_in_flights.php
+++ database/migrations/2025_03_19_162811_adding_index_column_in_flights.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('flights', function (Blueprint $table) {
-            $table->dropColumn('votes');
+            $table->string('email')->unique();
         });
     }
 
@@ -22,7 +22,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('flights', function (Blueprint $table) {
-            //
+            $table->dropColumn('email');
         });
     }
 };
