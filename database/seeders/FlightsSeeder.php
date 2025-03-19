a291b477f9777ecdf162a960be01cc0161bbd701
CarlosAndre2001ws <163556831+CarlosAndre2001ws@users.noreply.github.com>
2025-03-19 14:20:11 -0300

    Eloquent ORM

--- database/seeders/FlightsSeeder.php
+++ database/seeders/FlightsSeeder.php
@@ -2,8 +2,9 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
 
 class FlightsSeeder extends Seeder
 {
@@ -12,6 +13,11 @@ class FlightsSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        DB::table('flights')->insert([
+            'name' => Str::random(10),
+            'airline' => Str::random(10),
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
     }
 }
